9227babe3118109841308ab616855ec8e0241663
Aram Kim <nayulpapa0201@gmail.com>
2021-02-15 19:23:10 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const { response } = require("express");
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+mongoose
+  .connect(
+    "mongodb+srv://steveKim:ehxhfl80@react-blog.zqwi7.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("DB commected"))
+  .catch((err) => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(5000);
